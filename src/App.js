a92ba97aca44c72d6714e7e033a79ed5f258342b
Gadzeman <ostap778800@gmail.com>
2021-03-28 14:53:16 +0300

    lesson 1 components homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import CardProduct from "./components/CardProduct";
+import _ from "./components/news.css"
+import {Car} from "./components/Car";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const News = (props) => {
+    return(
+        <div className={"news"}>
+            <h1>{props.title}</h1>
+            <p>{props.events}</p>
+            <h3>{props.conslution}</h3>
+        </div>
+    )
 }
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className={"cardProducts"}>
+                    <CardProduct title={"iPhone"} represent={"Year: 2008. Memory: 16Gb"} price={"$ 13,99"} />
+                    <CardProduct title={"Tomato"} represent={"It's red and tasty"} price={"$ 0,99"}/>
+                </div>
+                <div className={"news"}>
+                    <News title={"TCH"} events={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias enim, excepturi illum itaque magnam nostrum ratione repellendus voluptatem?"} conslution={"Conslution"}/>
+                </div>
+                <div>
+                    <Car model={"Opel"} year={2006} engine={1.7} km={"260 km"}/>
+                </div>
+            </div>
+        );
+    };
+};
 
 export default App;
