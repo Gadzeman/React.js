2917e1849afbc3ae1538f4359a21b55a661b49f2
Gadzeman <ostap778800@gmail.com>
2021-03-29 21:40:14 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
 import React, {Component} from 'react';
-import _ from "./App.css"
+import {useState} from 'react';
 
-let Header = ({test}) => {
-    let counter = 0
-    return(
-        <header>
-            <h1>Test {test}</h1>
-            <button className={"button"}>cart {counter}</button>
-        </header>
-    )
-}
+function App () {
 
-class App extends Component {
-    render() {
-        let test = this.props.test
-        return (
-            <div>
-                {2 > Math.random() * 5 && <h1>Nice</h1>}
-                {5 > Math.random() * 7 ? <h2>True</h2> : <h2>False</h2>}
-                <Header test={test}/>
-            </div>
-        );
+    const [count, setCount] = useState(0)
+    const [isHeaderVisible, setIsHeaderVisible] = useState(true)
+    const countHandle = () => {
+        setCount(count + 1)
     }
+    const hideHeader = () => {
+        setIsHeaderVisible(!isHeaderVisible)
+    }
+
+    const [array, setArray] = useState(["hello", "react"])
+    const changeArrayValue = () => {
+        let newArray = [...array]
+        newArray[0] = Math.random()
+        setArray(newArray)
+    }
+
+    const [list, setList] = useState([
+        {"id": 1, "title": "hello", "text": "react"},
+        {"id": 2, "title": "what`s up", "text": "react"},
+        {"id": 3, "title": "how`s is going", "text": "react"},
+        {"id": 4, "title": "bye", "text": "react"}
+    ])
+
+    const Map = () => {
+        return(
+            <div>
+                {list.map((value, index) => (
+                    <div>
+                        <h4>{value.title}</h4>
+                        <h6>{value.text}</h6>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+
+    return(
+        <div>
+            <div>
+                {isHeaderVisible && <h1>{count}</h1>}
+                <button onClick={
+                    countHandle
+                }> Click on me </button>
+                <button onClick={
+                    hideHeader
+                }> Hide me </button>
+                <hr/>
+            </div>
+
+            <div>
+                <button onClick={
+                    changeArrayValue
+                }> Change value </button> <br/>
+                {array[0]} <br/>
+                {array[1]}
+                <hr/>
+            </div>
+
+            <div>
+                <Map />
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
