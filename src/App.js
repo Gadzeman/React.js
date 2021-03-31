fd429fb171ec7bedfe1a3ed904d10a1e2392a060
Gadzeman <ostap778800@gmail.com>
2021-04-01 00:15:56 +0300

    Not done homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React, {Component, useState, useEffect} from 'react';
 
-const App = () => {
-
-    // урл
-    // https://jsonplaceholder.typicode.com/
-    //
-    //     ендпоінти
-    //     /posts
-    //     /comments
-    //     /albums
-    //     /photos
-    //     /todos
-    //     /users
-    //
-    //
-    // потрібно створити логіку, яка задовільнить наступні вимоги
-    // в нас має бути 6 кнопок, які дозволяють нам переключатись між 'табами' (posts, comments, albums, photos, todos, users)
-    // дефолтно обрана таба- пости
-    // по кліку на кнопку ми повинні підтягнути відповідний список і зрендерити його через .map
-    // лише 1 список видимий одночасно
-    // потрібно створити 6 компонент, які займатимуться рендерінгом списків (отримуватимуть пропсами список)- PostList, CommentsList...
+function App ()  {
 
     return(
         <>
