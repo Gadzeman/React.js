b72ab21247c5043dc393046d84b4ba448a1e88e3
Gadzeman <ostap778800@gmail.com>
2021-04-03 00:50:42 +0300

    Working with inputs

--- src/App.js
+++ src/App.js
@@ -1,143 +1,76 @@
-import React, {Component, useState, useEffect} from 'react';
-
-const Tabs = ({tabs, selectedTab}) => {
-    return (
-        <div style={{
-            display: "flex"
-        }}>
-            {tabs.map(tab =>
-                <button
-                    key={tab.title}
-                    style={{
-                        background: selectedTab === tab.title ? "green" : "silver",
-                        flex: 1,
-                        height: "45px",
-                        fontSize: "20px"
-                    }}
-                    onClick={tab.tabHandle}
-                >
-                    {tab.title}
-                </button>
-            )}
-        </div>
-    )
-}
+import React from "react";
 
 function App () {
 
-    const PostsList = ({posts}) => {
-        return (
-            <div>
-                {posts.map(post => (
-                    <div key={post.id}>
-                        <h3>{post.title}</h3>
-                        <p>{post.body}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-    const CommentsList = ({comments}) => {
-        return (
-            <div>
-                {comments.map(comment => (
-                    <div key={comment.id}>
-                        <h3>{comment.name}</h3>
-                        <p>{comment.email}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-    const AlbumsList = ({albums}) => {
-        return (
-            <div>
-                {albums.map(album => (
-                    <div key={album.id}>
-                        <h3>{album.userId} - {album.id}</h3>
-                        <p>{album.title}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-    const PhotosList = ({photos}) => {
-        return (
-            <div>
-                {photos.map(photo => (
-                    <div key={photo.id}>
-                        <h3>{photo.id}</h3>
-                        <p>{photo.title}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-    const TodosList = ({todos}) => {
-        return (
-            <div>
-                {todos.map(todo => (
-                    <div key={todo.id}>
-                        <h3>{todo.id} (User: {todo.userId})</h3>
-                        <p>{todo.title}</p>
-                        <h4>Status: {todo.completed.toString()}</h4>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-    const UsersList = ({users}) => {
-        return (
-            <div>
-                {users.map(user => (
-                    <div key={user.id}>
-                        <h3>User: {user.name} --> {user.username}</h3>
-                        <p>{user.email}</p>
-                    </div>
-                ))}
-            </div>
-        )
-    }
+    const [firstName, setFirstName] = React.useState("")
 
-    const [list, setList] = useState([])
-
-    const tabs = [
-        {title: 'posts', tabHandle: () => {setSelectedTab(tabs[0].title, setList([]))}},
-        {title: 'comments', tabHandle: () => {setSelectedTab(tabs[1].title, setList([]))}},
-        {title: 'albums', tabHandle: () => {setSelectedTab(tabs[2].title, setList([]))}},
-        {title: 'photos', tabHandle: () => {setSelectedTab(tabs[3].title, setList([]))}},
-        {title: 'todos', tabHandle: () => {setSelectedTab(tabs[4].title, setList([]))}},
-        {title: 'users', tabHandle: () => {setSelectedTab(tabs[5].title, setList([]))}}
-    ]
-
-    const [selectedTab, setSelectedTab] = useState(tabs[0].title)
-
-    const urlBuilder = (resource) => `https://jsonplaceholder.typicode.com/${resource}`
-
-    const fetchData = async () => {
-        const url = urlBuilder(selectedTab)
-        const response = await fetch(url)
-        const data = await response.json()
-        setList(data)
+    const handleSubmit = () => {
+        alert(JSON.stringify({firstName}, null, 2))
     }
-
-    useEffect(() => {
-        fetchData()
-    }, [selectedTab])
 
     return (
-        <div>
-            <Tabs tabs={tabs} selectedTab={selectedTab}/>
-
-            {selectedTab === tabs[0].title && <PostsList posts={list} />}
-            {selectedTab === tabs[1].title && <CommentsList comments={list}/>}
-            {selectedTab === tabs[2].title && <AlbumsList albums={list}/>}
-            {selectedTab === tabs[3].title && <PhotosList photos={list}/>}
-            {selectedTab === tabs[4].title && <TodosList todos={list}/>}
-            {selectedTab === tabs[5].title && <UsersList users={list}/>}
-
+        <div style={{
+            textAlign: "center"
+        }}>
+            <h1>Inputs</h1>
+            <input value={firstName} onChange={({target: {value}}) => setFirstName(value)} type="text" name="firstName" placeholder="enter your first name"/>
+            <br/>
+            <br/>
+            <input type="text" name="lastName"  placeholder="enter your last name"/>
+            <br/>
+            <br/>
+            <input type="email" name="email"  placeholder="enter your email"/>
+            <br/>
+            <br/>
+            <input type="number" name="age"  placeholder="enter your age"/>
+            <br/>
+            <br/>
+            <input type="password" name="pass"  placeholder="enter your password"/>
+            <br/>
+            <br/>
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
 
-export default App;
+
+
+// ****************************Controlled Inputs***************************
+// function App () {
+//
+//     const firstName = React.useRef()
+//
+//     const onSubmit = (event) => {
+//         event.preventDefault() // prevent - запобігати (в даному випадку запогіаємо перезагрузки сторінки)
+//         console.log(firstName.current.value)
+//         firstName.current.value = ""
+//     }
+//
+//     return (
+//         <div style={{
+//             textAlign: "center"
+//         }}>
+//             <h1>Lecture: Inputs and type of inputs</h1>
+//             <form onSubmit={onSubmit}>
+//                 <input ref={firstName} type="text" name="firstName" placeholder="enter your first name"/>
+//                 <br/>
+//                 <br/>
+//                 <input type="text" name="lastName"  placeholder="enter your last name"/>
+//                 <br/>
+//                 <br/>
+//                 <input type="email" name="email"  placeholder="enter your email"/>
+//                 <br/>
+//                 <br/>
+//                 <input type="number" name="age"  placeholder="enter your age"/>
+//                 <br/>
+//                 <br/>
+//                 <input type="password" name="pass"  placeholder="enter your password"/>
+//                 <br/>
+//                 <br/>
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+export default App
