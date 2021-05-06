e7c2b02e4c9c0885b9637fb51c568e0c27093aee
Gadzeman <ostap778800@gmail.com>
2021-05-06 14:05:03 +0300

    .

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import React from "react"
+import Header from "./components/header"
+import Products from "./components/products";
 
 export default function App() {
   return (
     <div>
-
+        <Header />
+        <Products />
     </div>
   );
 }
